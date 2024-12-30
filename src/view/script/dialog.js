cc153c5d32fc2de0a7a0bdef9a3d6635434545cf
Mustafa Al-arify <m2024u775516215@gmail.com>
2024-12-30 21:03:01 +0300

    finish all Dialog

---
+++ src/view/script/dialog.js
@@ -0,0 +1,4 @@
+function openDialog(idDivDialog) {
+  const dialog = document.getElementById(idDivDialog);
+  dialog.style.display = dialog.style.display === "block" ? "none" : "block";
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      primaryDialog: "#4278ff",
+    }
   },
   plugins: [],
 }
