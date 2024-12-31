461d4bbf4d290891b3f6a5f770649f83a0df4712
Emad <emadalhammadi964@outlook.com>
2024-12-31 20:42:50 +0300

    add dialog

--- src/view/script/dialog.js
+++ src/view/script/dialog.js
@@ -1,4 +1,7 @@
-function openDialog(idDivDialog) {
+function openDialog(idDivDialog,isoverlay) {
   const dialog = document.getElementById(idDivDialog);
+  const overlay = document.getElementById(isoverlay);
   dialog.style.display = dialog.style.display === "block" ? "none" : "block";
+  overlay.style.display = overlay.style.display === "block" ? "none" : "block";
+  
 }
